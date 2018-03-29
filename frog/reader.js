675aabaaafc269fa6082efeb2e26c4661a0b24a2
Greg Burgreen <greg.burgreen@gmail.com>
2018-03-29 13:34:08 -0500

    revamp frog/reader.js

--- frog/reader.js
+++ frog/reader.js
@@ -1,88 +1,89 @@
-// frog-reader2.js
+// frog/reader.js
 
-//import 'vtk.js/Sources/favicon';
-//import vtkActor from 'vtk.js/Sources/Rendering/Core/Actor';
-//import vtkFullScreenRenderWindow from 'vtk.js/Sources/Rendering/Misc/FullScreenRenderWindow';
-//import vtkMapper from 'vtk.js/Sources/Rendering/Core/Mapper';
-//import vtkSTLReader from 'vtk.js/Sources/IO/Geometry/STLReader';
+// ----------------------------------------------------------------------------
+// parts to load
+// ----------------------------------------------------------------------------
+
+const part = []
+{
+  part.push('body')
+  part.push('wheels')
+}
+
+// ----------------------------------------------------------------------------
+// define controls
+// ----------------------------------------------------------------------------
+
+let controls = '<table>' 
+for( i in part)
+{
+  controls += '<tr>';
+  controls += '<td>'+part[i]+'</td>';
+  controls += '<td>';
+  controls += '<input class="_'+part[i]+'" type="checkbox" checked />';
+  controls += '</td>';
+  controls += '</tr>';
+}
+controls += '</table>' 
 
 // ----------------------------------------------------------------------------
 // main objects
 // ----------------------------------------------------------------------------
 
-const reader = vtk.IO.Geometry.vtkSTLReader.newInstance();
-const mapper = vtk.Rendering.Core.vtkMapper.newInstance({ scalarVisibility: false });
-const actor = vtk.Rendering.Core.vtkActor.newInstance();
+const reader = []
+const mapper = []
+const actor = []
+for( i in part )
+{
+  reader.push( vtk.IO.Geometry.vtkSTLReader.newInstance() );
+  mapper.push( vtk.Rendering.Core.vtkMapper.newInstance({ scalarVisibility: false }) );
+  actor.push(  vtk.Rendering.Core.vtkActor.newInstance() );
+  actor[i].setMapper( mapper[i] );
+  mapper[i].setInputConnection( reader[i].getOutputPort() );
+}
 
-const reader2 = vtk.IO.Geometry.vtkSTLReader.newInstance();
-const mapper2 = vtk.Rendering.Core.vtkMapper.newInstance({ scalarVisibility: false });
-const actor2 = vtk.Rendering.Core.vtkActor.newInstance();
+const fullScreenRenderer = vtk.Rendering.Misc.vtkFullScreenRenderWindow.newInstance();
+const renderer = fullScreenRenderer.getRenderer();
+const renderWindow = fullScreenRenderer.getRenderWindow();
 
-actor.setMapper(mapper);
-mapper.setInputConnection(reader.getOutputPort());
+const resetCamera = renderer.resetCamera;
+const render = renderWindow.render;
 
-actor2.setMapper(mapper2);
-mapper2.setInputConnection(reader2.getOutputPort());
-
-
-// ----------------------------------------------------------------------------
+fullScreenRenderer.addController(controls);
 
 function update() 
 {
-  const fullScreenRenderer = vtk.Rendering.Misc.vtkFullScreenRenderWindow.newInstance();
-  const renderer = fullScreenRenderer.getRenderer();
-  const renderWindow = fullScreenRenderer.getRenderWindow();
-
-  const resetCamera = renderer.resetCamera;
-  const render = renderWindow.render;
-
-  renderer.addActor(actor);
-  renderer.addActor(actor2);
+  for( i in actor )
+  {
+    let a = actor[i];
+    renderer.addActor(a);
+  }
   resetCamera();
   render();
 }
 
 // ----------------------------------------------------------------------------
-// Use a file reader to load a local file
+// load parts and display
 // ----------------------------------------------------------------------------
 
-/*
-const myContainer = document.querySelector('body');
-const fileContainer = document.createElement('div');
-fileContainer.innerHTML = '<input type="file" class="file"/>';
-myContainer.appendChild(fileContainer);
+const promises = []
+for( i in part )
+{
+  promises.push( reader[i].setUrl('data/'+part[i]+'.stl', { binary: true }) );
+}
+Promise.all(promises).then( update );
 
-const fileInput = fileContainer.querySelector('input');
+// ----------------------------------------------------------------------------
+// add control actions
+// ----------------------------------------------------------------------------
 
-function handleFile(event) {
-  event.preventDefault();
-  const dataTransfer = event.dataTransfer;
-  const files = event.target.files || dataTransfer.files;
-  if (files.length === 1) {
-    myContainer.removeChild(fileContainer);
-    const fileReader = new FileReader();
-    fileReader.onload = function onLoad(e) {
-      reader.parseBinary(fileReader.result);
-      update();
-    };
-    fileReader.readAsArrayBuffer(files[0]);
-  }
+for( i in part )
+{
+  const s = '._'+part[i];
+  const a = actor[i];
+  document.querySelector(s).addEventListener('change', (e) => {
+    a.setVisibility(!!e.target.checked);
+    render();
+  });
 }
 
-fileInput.addEventListener('change', handleFile);
-*/
-
-// ----------------------------------------------------------------------------
-// Use the reader to download a file
-// ----------------------------------------------------------------------------
-
-  //reader.setUrl(`${__BASE_PATH__}/data/stl/segmentation.stl`, { binary: true }).then(update);
-  //reader.setUrl(`frog/wheels.stl`, { binary: true }).then(update);
-  //reader2.setUrl(`frog/body.stl`, { binary: true }).then(update);
-reader.setUrl(`wheels.stl`, { binary: true })
-.then(
-  reader2.setUrl(`body.stl`, { binary: true })
-  .then(
-    update
-  )
-);
